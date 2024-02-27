7730738d9601e9709d148b7c8113d9081bee9eec
Avinash Anand <82769201+AvinashAnand02@users.noreply.github.com>
2024-02-27 18:49:35 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let menu = document.querySelector("#menu-bars");
+let header = document.querySelector("header");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  header.classList.remove("active");
+};
+
+var typed = new Typed(".typing-text", {
+  strings: ["front end developer", "web designer", "web developer"],
+  loop: true,
+  typeSpeed: 150,
+});
+
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+  max: 25,
+});
+
+async function fetchData(type = "skills") {
+  let response;
+  type === "skills"
+    ? (response = await fetch("skills.json"))
+    : (response = await fetch("./projects/projects.json"));
+  const data = await response.json();
+  return data;
+}
+
+function showSkills(skills) {
+  let skillsContainer = document.getElementById("skillsContainer");
+  let skillHTML = "";
+  skills.forEach((skill) => {
+    skillHTML += `
+        <div class="bar">
+              <div class="info">
+                <img src=${skill.icon} alt="skill" />
+                <span>${skill.name}</span>
+              </div>
+            </div>`;
+  });
+  skillsContainer.innerHTML = skillHTML;
+}
+
+fetchData().then((data) => {
+  showSkills(data);
+});
